9c48c0961ab9a4c4dd74bf03bb08307a42756534
Hari Om Aggrawal <hari.aggrawal@kitemedical.ie>
2022-09-03 16:09:25 +0530

    generate points files

--- gen_datasets.py
+++ gen_datasets.py
@@ -9,7 +9,7 @@ random.seed(0)
 file_path = '../data/USD/annotations/'
 wlimages = np.concatenate(([5], np.linspace(10, 90, 9)))
 
-# create USD-50: randomly choose 50% images
+# reading USD-50: randomly choosen 50% images
 df = pd.read_csv(os.path.join(file_path, 'train_usd50.csv'))
 # unique image names
 udf = df['image'].unique()
@@ -17,6 +17,19 @@ nimg = udf.shape[0]
 
 for wl in wlimages:
     idx = random.sample(range(0, nimg), math.ceil(nimg*wl/100))
+
+    # well-labeled images
     ndf = df[df['image'].isin(udf[idx])]
     ndf.to_csv(os.path.join(file_path, 'train_usd50_wl' + str(int(wl)) + '.csv'), index=False)
 
+    # well-labeled to point annotation (replace box with points)
+    idx = list(set(range(0,nimg)) - set(idx))
+    ndf = df[df['image'].isin(udf[idx])]
+
+    xc = 0.5 * (ndf.xmin.values + ndf.xmax.values)
+    yc = 0.5 * (ndf.ymin.values + ndf.ymax.values)
+    ndf = ndf.assign(xmin=xc)
+    ndf = ndf.assign(xmax=xc)
+    ndf = ndf.assign(ymin=yc)
+    ndf = ndf.assign(ymax=yc)
+    ndf.to_csv(os.path.join(file_path, 'train_usd50_pt' + str(int(100 - wl)) + '.csv'), index=False)
